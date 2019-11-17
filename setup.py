70e8fb5b7aebf10c2f912cc9573d0db54d44d3de
ProGamerGov <ProGamerGov@users.noreply.github.com>
2019-11-17 16:14:21 -0700

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="neural-tools",
-    version="0.0.1",
+    version="0.0.2",
     author="ProGamerGov",
     description="Luminance and histogram matching tools",
     long_description=long_description,
